59645a7f1cf7ce73ab11ee98d2794c5677aad1b2
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 17:20:38 +0530

    [satya] added- Main Program file

---
+++ Day2BRP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day2BRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumOfTwoDice s = new SumOfTwoDice();
+            s.AddDice();
+
+            IntOpt i = new IntOpt();
+            i.AddInput();
+
+            DoubleOpt d = new DoubleOpt();
+            d.CheckOperation();
+
+            Distance distance = new Distance();
+            distance.FindDistance();
+        }
+    }
+}
