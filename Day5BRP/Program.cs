0834ebabadc7bba9a5f9674da5e4d579105ed03b
BootCampBL <satyabhandari43@gmail.com>
2021-09-29 20:20:52 +0530

    [satya] added- Main Program File

---
+++ Day5BRP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day5BRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin f = new FlipCoin();
+            f.calculatePercentage();
+
+            PowerOf2 p = new PowerOf2();
+            p.printPowerOfTwo();
+
+            Console.ReadLine();
+        }
+    }
+}
