866aff247a2a73240ec77fead889fce59efd962e
BootCampBL <satyabhandari43@gmail.com>
2021-09-29 20:10:09 +0530

    [satya] added- method to print cosine and sine value of a degree

---
+++ Day4BRP/TrignometricFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4BRP
+{
+    class TrignometricFunctions
+    {
+        public void TrignometricFunction()
+        {
+            Console.WriteLine("---------- Trignometric Function------------");
+
+            double angleInDegree;
+            Console.WriteLine("Enter the angle in degrees =  ");
+            angleInDegree = Convert.ToInt32(Console.ReadLine());
+
+
+            double angleInRadian = angleInDegree * Math.PI / 180;
+
+
+            double sine = Math.Sin(angleInRadian);
+            Console.WriteLine("The value of sine  {0} is {1} ", +angleInDegree, +sine);
+
+            Console.WriteLine();
+
+            double cos = Math.Cos(angleInRadian);
+            Console.WriteLine("The value of cosine {0} is {1}  ", +angleInDegree, +cos);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+   
