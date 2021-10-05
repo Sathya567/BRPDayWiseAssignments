1c2d1d22d031f34a77678ffafa065c4f901f8853
BootCampBL <satyabhandari43@gmail.com>
2021-10-05 07:23:30 +0530

    [satya] added- method to print the prime factors of a number.

---
+++ Day7BRP/Primefactors.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7BRP
+{
+    class Primefactors
+    {
+        public void findingPrimeFactors()
+        {
+            int Num;
+            Console.WriteLine("Enter Number of which you want to print factors =");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Prime factors are ");
+            for (int i = 2; i < Num; i++)
+            {
+
+                if (Num % i == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+        }
+    }
+}
+    
