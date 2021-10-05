5cd5796d9fba18d011d4371a931d451502c3d2ce
BootCampBL <satyabhandari43@gmail.com>
2021-10-05 07:25:08 +0530

    [satya] added- Main Program File

---
+++ Day7BRP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7BRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primefactors primeFactors = new Primefactors();
+            primeFactors.findingPrimeFactors();
+
+            SecondLargestAndSmallest secondLargest = new SecondLargestAndSmallest();
+            secondLargest.FindSecondLargest();
+
+
+            Binary binary = new Binary();
+            binary.findingBinary();
+        }
+
+    }
+}
+   
