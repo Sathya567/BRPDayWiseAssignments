59d9cbe715c7e07e631400d82758625c7ad11fac
BootCampBL <satyabhandari43@gmail.com>
2021-10-05 07:20:29 +0530

    [satya] added- Main Method file

---
+++ Day6BRP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day6BRP
+{
+    class Program
+    {
+        
+           
+
+            static void Main(string[] args)
+            {
+
+                PrimeNumber p = new PrimeNumber();
+                p.PrintPrimeNumber();
+
+
+                RollDie roll = new RollDie();
+                roll.findMaximum();
+
+                RepeatedNumber repeatedNumber = new RepeatedNumber();
+                repeatedNumber.FindRepeatedNumber();
+            }
+        
+    }
+}
+   
